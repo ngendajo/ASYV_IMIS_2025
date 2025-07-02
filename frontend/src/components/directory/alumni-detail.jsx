c79ec6b95062220c08a63a2a697285c8c170fc38
unknown <kajalm@andrew.cmu.edu>
2025-07-02 11:09:18 +0200

    view profile from directory

--- frontend/src/components/directory/alumni-detail.jsx
+++ frontend/src/components/directory/alumni-detail.jsx
@@ -4,6 +4,7 @@ import useAuth from '../../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import baseUrl from '../../api/baseUrl';
+import ProfileCard from '../profile/profile-card';
 
 const AlumniDetail = ({ selectedAlumni, handleClear }) => {
   const navigate = useNavigate();
@@ -11,49 +12,53 @@ const AlumniDetail = ({ selectedAlumni, handleClear }) => {
 
   if (!selectedAlumni) return null;
 
+  const isStaff = auth.user?.is_crc || auth.user?.is_superuser;
+
   const combinationStyle = (combination) => combination?.replace(/-/g, ', ') || '';
 
   const handleViewClick = () => {
     console.log("alumni user id", selectedAlumni.user_id);
     navigate("/personal_profile", {
-    state: { userId: selectedAlumni.user_id }
-});
+      state: { userId: selectedAlumni.user_id }
+    });
   };
 
   return (
     <div className="alumni-detail-content">
-      <img src={selectedAlumni.profilePic} alt="Profile" className="detail-pic" />
-      <div className="detail-first-name">{selectedAlumni.firstName}</div>
-      <div className="detail-last-name">{selectedAlumni.lastName}</div>
-      {(auth.user?.is_crc || auth.user?.is_superuser) && (
-          <div>
-            <div className="detail-contact-info">{selectedAlumni.email}</div>
-            <div className="detail-contact-info">{selectedAlumni.phone}</div>
-          </div>
-        )}
+      {isStaff ? (
+        <>
+          <ProfileCard propId={selectedAlumni.user_id} />
+        </>
+      ) : (
+        <>
+          <img src={selectedAlumni.profilePic} alt="Profile" className="detail-pic" />
+          <div className="detail-first-name">{selectedAlumni.firstName}</div>
+          <div className="detail-last-name">{selectedAlumni.lastName}</div>
 
-      <div className="detail-grid">
-        <div className="DetailTitle">ASYV Grade</div>
-        <div className="DetailValue">{selectedAlumni.grade}</div>
-        <div className="DetailTitle">ASYV Family</div>
-        <div className="DetailValue">{selectedAlumni.family}</div>
-        <div className="DetailTitle">Combination</div>
-        <div className="DetailValue">{combinationStyle(selectedAlumni.combination)}</div>
-        <div className="DetailTitle">Job Industry</div>
-        <div className="DetailValue">{selectedAlumni.industry}</div>
-        <div className="DetailTitle">Further Education</div>
-        <div className="DetailValue">{selectedAlumni.further_education}</div>
-      </div>
+          {(auth.user?.is_crc || auth.user?.is_superuser) && (
+            <div>
+              <div className="detail-contact-info">{selectedAlumni.email}</div>
+              <div className="detail-contact-info">{selectedAlumni.phone}</div>
+            </div>
+          )}
 
-     {(auth.user?.is_crc || auth.user?.is_superuser) && (
-          <div className="alumni-detail-button">
-            <button onClick={handleViewClick} className="alumni-view-button">
-              View
-            </button>
+          <div className="detail-grid">
+            <div className="DetailTitle">ASYV Grade</div>
+            <div className="DetailValue">{selectedAlumni.grade}</div>
+            <div className="DetailTitle">ASYV Family</div>
+            <div className="DetailValue">{selectedAlumni.family}</div>
+            <div className="DetailTitle">Combination</div>
+            <div className="DetailValue">{combinationStyle(selectedAlumni.combination)}</div>
+            <div className="DetailTitle">Job Industry</div>
+            <div className="DetailValue">{selectedAlumni.industry}</div>
+            <div className="DetailTitle">Further Education</div>
+            <div className="DetailValue">{selectedAlumni.further_education}</div>
           </div>
-        )}
+        </>
+      )}
     </div>
   );
 };
+
 
 export default AlumniDetail;
